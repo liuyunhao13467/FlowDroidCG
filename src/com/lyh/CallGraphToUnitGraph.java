9567f70a2e3c06678b4139af7f34ab0ffbf95e07
liuyunhao13467 <1075538266@qq.com>
2016-12-26 16:06:33 +0800

    新增将CallGraph转化为dot文件的类：CallGraphToUnitGraph

--- src/com/lyh/CallGraphToUnitGraph.java
+++ src/com/lyh/CallGraphToUnitGraph.java
@@ -10,28 +10,26 @@ import soot.PackManager;
 import soot.PhaseOptions;
 import soot.Scene;
 import soot.SootMethod;
-import soot.coffi.CFG;
 import soot.jimple.infoflow.android.SetupApplication;
 import soot.jimple.spark.SparkTransformer;
 import soot.jimple.toolkits.callgraph.CallGraph;
 import soot.jimple.toolkits.callgraph.Targets;
 import soot.options.Options;
-import soot.toolkits.graph.pdg.EnhancedBlockGraph;
-import soot.util.cfgcmd.CFGToDotGraph;
+import soot.util.dot.DotGraph;
+import soot.util.dot.DotGraphNode;
 
-public class CGGenerator {
+public class CallGraphToUnitGraph {
+	//dot图
+	private static DotGraph dotGraph = new DotGraph("example1");
 	//设置android的jar包目录
     public final static String jarPath = "D:\\adt-bundle-windows-x86_64-20140702\\sdk\\platforms";
     //设置要分析的APK文件
     public final static String apk = "D:\\androidworkspace\\ContactReader.apk";
-
+    //这个数据结构中存储已经访问过的callgraph节点
     private static Map<String,Boolean> visited = new HashMap<String,Boolean>();
-    private static CGExporter cge = new CGExporter();
-    
-    private static CFGToDotGraph cfgToDG = new CFGToDotGraph();
-
 
     public static void main(String[] args){
+    	//找到要分析的apk文件
         SetupApplication app = new SetupApplication(jarPath, apk);
         try{
             //计算APK的入口点，这一步导入的文件是Flowdroid进行污点分析的时候需要的，这里直接新建一个空文件即可
@@ -39,8 +37,10 @@ public class CGGenerator {
         }catch(Exception e){
             e.printStackTrace();
         }
-        soot.G.reset();//重新实例化
+        //重新实例化
+        soot.G.reset();
 
+        //开始各种设置：
         Options.v().set_src_prec(Options.src_prec_apk);
         Options.v().set_process_dir(Collections.singletonList(apk));
         Options.v().set_force_android_jar(jarPath + "\\android-19\\android.jar");
@@ -52,7 +52,7 @@ public class CGGenerator {
         Options.v().set_app(true);
         enableSpark();
         Scene.v().loadNecessaryClasses();
-
+        //对于Android程序来说，创建DummyMain函数
         SootMethod entryPoint = app.getEntryPointCreator().createDummyMain();
         Options.v().set_main_class(entryPoint.getSignature());
         Scene.v().setEntryPoints(Collections.singletonList(entryPoint));
@@ -63,23 +63,40 @@ public class CGGenerator {
         //可视化函数调用图
         visit(cg,entryPoint);
         //导出函数调用图
-        cge.exportMIG("flowdroidCFG4.gexf", "C:\\Users\\Administrator\\Desktop\\result");
+        //cge.exportMIG("flowdroidCFG4.gexf", "C:\\Users\\Administrator\\Desktop\\result");
+        exportGraph("example1", "C:\\Users\\Administrator\\Desktop\\result");
     }
-    //可视化函数调用图的函数
-    private static void visit(CallGraph cg,SootMethod m){
-        //在soot中，函数的signature就是由该函数的类名，函数名，参数类型，以及返回值类型组成的字符串
-        String identifier = m.getSignature();
-        //记录是否已经处理过该点
+	
+	/**
+	 * 生成dot文件
+	 * @param graphName
+	 * @param storeDir
+	 */
+	public static void exportGraph(String graphName, String storeDir){
+		String outPath = storeDir + "/" + graphName + DotGraph.DOT_EXTENSION;
+		dotGraph.plot(outPath);
+	}
+	
+	/**
+	 * 可视化函数调用图
+	 * @param cg 函数的callgraph
+	 * @param m 我们要分析的system的entry point
+	 */
+	private static void visit(CallGraph cg,SootMethod m){
+		//在soot中，函数的signature就是由该函数的类名，函数名，参数类型，以及返回值类型组成的字符串
+        String methodId = m.getSignature();
+        //记录已经处理过该点
         visited.put(m.getSignature(), true);
         //以函数的signature为label在图中添加该节点
-        cge.createNode(m.getSignature());
+        dotGraph.drawNode(methodId);
+        //下面可以对上面刚生成的DotNode设置一些参数(Attribute)
         
-        //获取调用该函数的函数
-        Iterator<MethodOrMethodContext> ptargets = new Targets(cg.edgesInto(m));
-        if(ptargets != null){
-            while(ptargets.hasNext())
+        //获取调用该函数的函数(callersOfM意为调用传入参数：SootMethod m的函数，即SootMethod m的调用者们)
+        Iterator<MethodOrMethodContext> callersOfM = new Targets(cg.edgesInto(m));
+        if(callersOfM != null){
+            while(callersOfM.hasNext())
             {
-                SootMethod p = (SootMethod) ptargets.next();
+                SootMethod p = (SootMethod) callersOfM.next();
                 if(p == null){
                     System.out.println("p is null");
                 }
@@ -88,7 +105,8 @@ public class CGGenerator {
                 }
             }
         }
-        //获取该函数调用的函数
+        
+        //获取该函数调用的函数，即被传入参数SootMethod m调用的函数们（calleesOfM）
         Iterator<MethodOrMethodContext> ctargets = new Targets(cg.edgesOutOf(m));
         if(ctargets != null){
             while(ctargets.hasNext())
@@ -98,18 +116,21 @@ public class CGGenerator {
                     System.out.println("c is null");
                 }
                 //将被调用的函数加入图中
-                cge.createNode(c.getSignature());
+                dotGraph.drawNode(c.getSignature());
                 //添加一条指向该被调函数的边
-                cge.linkNodeByID(identifier, c.getSignature());
+                dotGraph.drawEdge(methodId, c.getSignature());
                 if(!visited.containsKey(c.getSignature())){
                     //递归
                     visit(cg,c);
                 }
             }
         }
-    }
-    
-    public static void enableSpark() {
+	}
+	
+	/**
+	 * spark相关设置
+	 */
+	public static void enableSpark() {
 		//只是针对spark方面的配置。
 		PhaseOptions.v().setPhaseOption("cg.spark", "enabled:true");
 		HashMap<String, String> opt = new HashMap<String, String>();//TODO 为什么设置之后无效？
@@ -124,5 +145,4 @@ public class CGGenerator {
 		opt.put("apponly", "true");
 		SparkTransformer.v().transform("", opt);
 	}
-
 }
