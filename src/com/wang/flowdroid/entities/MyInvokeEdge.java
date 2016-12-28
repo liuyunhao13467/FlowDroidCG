8763d21e371aab5adcb1a7298bf6e48f1ad59be1
wang <415249614@qq.com>
2016-12-28 14:36:58 +0800

    清理

--- src/com/wang/flowdroid/entities/MyInvokeEdge.java
+++
@@ -1,9 +0,0 @@
-package com.wang.flowdroid.entities;
-
-public class MyInvokeEdge extends MyEdge{
-
-	public MyInvokeEdge(Integer ip, Integer src, Integer tgt) {
-		super(ip, src, tgt);
-	}
-
-}
--- src/com/wang/flowdroid/entities/MyUnitEdge.java
+++
@@ -1,9 +0,0 @@
-package com.wang.flowdroid.entities;
-
-public class MyUnitEdge extends MyEdge {
-
-	public MyUnitEdge(Integer ip, Integer src, Integer tgt) {
-		super(ip, src, tgt);
-	}
-
-}
